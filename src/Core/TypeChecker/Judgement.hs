a99caee9c3e6f10a39ca6ee16c380e7f989cdb2e
Boarders <callan.mcgill@gmail.com>
2020-05-28 12:47:07 -0400

    Add the rest of the judgements

--- src/Core/TypeChecker/Judgement.hs
+++ src/Core/TypeChecker/Judgement.hs
@@ -10,6 +10,12 @@ import Core.Context
 import Core.Expression
 import Control.Monad.Except
 import Data.Tree
+import Prelude hiding (lookup, splitAt)
+
+
+data TypeHint =
+    HintArr
+  | HintUnit
 
 data JudgementError v =
     SubTypeError
@@ -21,6 +27,8 @@ data JudgementError v =
   | InstaniationRError (Type v) v
   | InstaniationLError v (Type v)
   | ScopeMarkError v (Context v)
+  | TypeError TypeHint (Term v) (Type v)
+  | ApplyError (Context v) (Type v) (Term v)
 
 instance VarError v (JudgementError v) where
   varErr = VarError
@@ -194,11 +202,120 @@ instantiateR ctxt (ForAll b ty_b) exA =
       exB      = fresh ctxt
       exVarB   = ExVar exB
       exContext = ctxt :|> ScopeMark exB :|> (ConExVar exB)
-      instTy_b = substitute b exVarB ty_b
+      instTy_b = subTyVar b exVarB ty_b
     exBCon <- instantiateR exContext instTy_b exA
     removeScopeSuffix (\v c -> ScopeMarkError v c) exA exBCon
 instantiateR _ ty var = throwError (InstaniationRError ty var)
 
+
+infer
+  :: (MonadError (JudgementError v) m, Fresh v, Eq v, Ord v)
+  => Context v -> Term v -> m (Type v, Context v)
+  -- Var
+infer ctxt (Var v) =
+  do
+    case lookupVar v ctxt of
+      Just ty -> pure (ty, ctxt)
+      Nothing -> throwError (varErr v ctxt)
+-- 1I=>
+infer ctxt Unit = pure (TyUnit, ctxt)
+-- Anno
+infer ctxt (Ann term ty) =
+  do
+    outputCtxt <- check ctxt term ty
+    pure (ty, outputCtxt)
+-- ->I=>
+infer ctxt (Lam var body) =
+  do
+    let alphaFresh = fresh ctxt
+    let alphaEx    = ConExVar alphaFresh
+    let alphaTy    = ExVar alphaFresh
+    let betaFresh  = fresh (ctxt :|> alphaEx)
+    let betaEx     = ConExVar betaFresh
+    let betaTy     = ExVar betaFresh
+    let varTy      = ConTerm var (ExVar alphaFresh)
+    let extendCtxt = ctxt :|> alphaEx :|> betaEx :|> varTy
+    bodyCtxt <- check extendCtxt body betaTy
+    (outputCtxt, _) <- splitAt (\v c -> varErr v c) varTy bodyCtxt
+    pure ((TyArr alphaTy betaTy), outputCtxt)
+infer ctxt (App e1 e2) =
+  do
+    (funTy, funCtxt) <- infer ctxt e1
+    let appTy = applyContext funCtxt funTy
+    apply funCtxt appTy e2
+
+check 
+  :: (MonadError (JudgementError v) m, Fresh v, Eq v, Ord v)
+  => Context v -> Term v ->  Type v -> m (Context v)
+-- 1I  
+check ctxt Unit ty =
+  do
+    case ty == TyUnit of
+      True  -> pure ctxt
+      False -> throwError $ TypeError HintUnit Unit ty
+-- ∀I
+check ctxt term (ForAll a ty_a) =
+  do
+    let exVar  = ConExVar a
+    let exCtxt = ctxt :|> exVar
+    exSolved <- check exCtxt term ty_a
+    (prefix, _) <- splitAt (\v c -> varErr v c) exVar exSolved
+    pure prefix
+-- ->I
+check ctxt term@(Lam var body) ty =
+  case isArr ty of
+    Nothing -> throwError (TypeError HintArr term ty)
+    Just (TyArr a b) ->
+      do
+        let varDef  = ConTerm var a
+        let varCtxt = ctxt :|> varDef
+        freeVarCtxt <- check varCtxt body b
+        (prefix, _) <- splitAt (\v c -> varErr v c) varDef freeVarCtxt
+        pure prefix
+    _ -> error "isArr: gave incorrect arrow"
+-- Sub
+check ctxt term supTy =
+  do
+    (subTy, outputCtxt) <- infer ctxt term
+    let instSubTy = applyContext outputCtxt subTy
+    let instSupTy = applyContext outputCtxt supTy
+    subTyCtxt <- subtype outputCtxt instSubTy instSupTy
+    pure subTyCtxt
+    
+    
+
+apply 
+  :: (MonadError (JudgementError v) m, Fresh v, Eq v, Ord v)
+  => Context v -> Type v -> Term v -> m (Type v, Context v)
+-- ∀App
+apply ctxt (ForAll a ty_a) term =
+  do
+    let freshEx = fresh ctxt
+    let exVar   = ConExVar freshEx
+    let exTyVar = ExVar freshEx
+    let exCtxt = ctxt :|> exVar
+    let exTy   = subTyVar a exTyVar ty_a
+    apply exCtxt exTy term
+-- αApp
+apply ctxt (ExVar exA) term =
+  do
+    handleVar (hasExVar exA ctxt) (VarError exA ctxt) (pure ())
+    let
+      exName1 = fresh ctxt
+      exVar1  = ConExVar exName1
+      exTy1   = ExVar exName1
+      exName2 = fresh (ctxt :|> exVar1)
+      exVar2  = ConExVar exName2
+      exTy2   = ExVar exName2
+      arrowDef = ConExDef exA (MonoArr (MonoExVar exName1) (MonoExVar exName2))
+      newCtxt = ctxt :|> exVar1 :|> exVar2 :|> arrowDef
+    solved <- check newCtxt term exTy1
+    pure (exTy2, solved)
+apply ctxt (TyArr ty1 ty2) term =
+  do
+    solved <- check ctxt term ty1
+    pure (ty2, solved)
+apply ctxt ty term = throwError (ApplyError ctxt ty term)
 
 
 data JudgmentType v =
@@ -208,7 +325,7 @@ data JudgmentType v =
   | Sub   (Context v)   (Term v) (Type v) (Context v)
   | Inst  (Context v)   (Term v) (Type v) (Context v)  
      -- input context   fun     arg     infer type  context   
-  | App   (Context v)   (Term v) (Term v) (Type v)    (Context v)
+  | AppJ  (Context v)   (Term v) (Term v) (Type v)    (Context v)
 
 
 type Judgment v = (JudgmentType v, String)
